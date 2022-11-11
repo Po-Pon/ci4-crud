a810b6537a6bc562d9689c62ce4ddfe1c6cdd55f
phalat wongsitthipornrung <phalat18@gmail.com>
2022-11-11 15:25:59 +0700

    Update namelist.php

--- app/Views/namelist.php
+++ app/Views/namelist.php
@@ -1,18 +1,58 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>namelist</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-</head>
-<body>
-    <div class="contrainer">
-        <h1>Hello World</h1>
-    </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Codeigniter Crud</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<div class="container mt-4">
+<h1>Codeigniter Crud</h1>
+    <hr>
+    <div class="d-flex justify-content-end">
+        <a href="<?php echo site_url('/addname') ?>" class="btn btn-primary">Add a Name & email</a>
+	</div>
+    <?php
+     if(isset($_SESSION['msg'])){
+        echo $_SESSION['msg'];
+      }
+     ?>
+  <div class="mt-3">
+     <table class="table table-bordered" id="users-list">
+       <thead>
+          <tr>
+             <th>User Id</th>
+             <th>Name</th>
+             <th>Email</th>
+             <th>Action</th>
+          </tr>
+       </thead>
+       <tbody>
+          <?php if($users): ?>
+          <?php foreach($users as $user): ?>
+          <tr>
+             <td><?php echo $user['id']; ?></td>
+             <td><?php echo $user['name']; ?></td>
+             <td><?php echo $user['email']; ?></td>
+             <td>
+              <a href="<?php echo base_url('editnames/'.$user['id']);?>" class="btn btn-warning">Edit</a>
+              <a href="<?php echo base_url('delete/'.$user['id']);?>" class="btn btn-danger">Delete</a>
+              </td>
+          </tr>
+         <?php endforeach; ?>
+         <?php endif; ?>
+       </tbody>
+     </table>
+  </div>
+</div>
+ 
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready( function () {
+      $('#users-list').DataTable();
+  } );
+</script>
 </body>
 </html>
